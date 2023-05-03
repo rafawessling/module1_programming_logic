3e0f4826d35129bb92ecec3f7e3916f29746a347
rafawessling <rafaoening@gmail.com>
2023-05-03 21:49:29 +0200

    Updating exercise 06 with Math.min

--- 06_cacula_dos_adultos/index.js
+++ 06_cacula_dos_adultos/index.js
@@ -3,14 +3,9 @@ const solucao = lista => {
         const participantes = lista.filter(idade => {
             return idade >= 18;
         });
-        let menorIdade = participantes[0];
 
         if (participantes.length !== 0) {
-            for (let item of participantes) {
-                if (item < menorIdade) {
-                    menorIdade = item;
-                }
-            }
+            let menorIdade = Math.min(...participantes);
             console.log(menorIdade);
         } else {
             console.log('CRESCA E APARECA');
