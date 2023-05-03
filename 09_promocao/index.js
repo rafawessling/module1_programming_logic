58d3eed65aa9367f5291175a63c3c1a1d81f2acc
rafawessling <rafaoening@gmail.com>
2023-05-04 00:09:21 +0200

    Adding exercise 09

---
+++ 09_promocao/index.js
@@ -0,0 +1,20 @@
+function solucao(precos) {
+    if (precos.length <= 100) {
+        const total = precos.reduce((acumulador, item) => {
+            return acumulador + item;
+        });
+
+        let totalComDesconto = 0;
+        if (precos.length > 2) {
+            let menorValor = 0;
+            menorValor = Math.min(...precos);
+            totalComDesconto = total - menorValor + menorValor * 0.5;
+
+            console.log(totalComDesconto);
+        } else {
+            console.log(total);
+        }
+    }
+}
+
+solucao([200, 150, 50, 100]);
