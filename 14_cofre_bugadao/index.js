7ef452022af76732b6d08c3f8c85e50de1ea6f0d
rafawessling <rafaoening@gmail.com>
2023-05-12 00:30:03 +0200

    Adding another solution

--- 14_cofre_bugadao/index.js
+++ 14_cofre_bugadao/index.js
@@ -1,36 +1,56 @@
+// function processData(input) {
+//     const linhas = input.trim().split('\n');
+
+//     let senha = linhas[0].split('');
+//     let digitado = linhas[1].split('');
+//     let verificar = [];
+
+//     let letraAtual = digitado.findIndex(item => {
+//         return item === senha[0];
+//     });
+//     if (letraAtual !== -1) {
+//         verificar.push(digitado[letraAtual]);
+//     }
+
+//     for (let i = 1; i < senha.length; i++) {
+//         const proximaLetra = digitado.findIndex(item => {
+//             return item === senha[i];
+//         });
+//         if (proximaLetra === -1) {
+//             break;
+//         } else if (letraAtual < proximaLetra) {
+//             verificar.push(digitado[proximaLetra]);
+//             letraAtual = proximaLetra;
+//         } else {
+//             delete digitado[proximaLetra];
+//             i--;
+//         }
+//     }
+//     if (verificar.join('') === senha.join('')) {
+//         console.log('SIM');
+//     } else {
+//         console.log('NAO');
+//     }
+// }
+
+//---------------------------------------------------------------------
+//Alternativa:
 function processData(input) {
-    const linhas = input.split('\n');
+    const linhas = input.trim().split('\n');
 
-    let senha = linhas[0].split('');
-    let digitado = linhas[1].split('');
-    let verificar = [];
+    const senha = linhas[0];
+    let digitado = linhas[1];
 
-    let letraAtual = digitado.findIndex(item => {
-        return item === senha[0];
-    });
-    if (letraAtual !== -1) {
-        verificar.push(digitado[letraAtual]);
-    }
-
-    for (let i = 1; i < senha.length; i++) {
-        const proximaLetra = digitado.findIndex(item => {
-            return item === senha[i];
-        });
-        if (proximaLetra === -1) {
-            break;
-        } else if (letraAtual < proximaLetra) {
-            verificar.push(digitado[proximaLetra]);
-            letraAtual = proximaLetra;
+    for (let item of senha) {
+        let index = digitado.indexOf(item);
+        if (index === -1) {
+            console.log('NAO');
+            return;
         } else {
-            delete digitado[proximaLetra];
-            i--;
+            digitado = digitado.slice(index + 1);
         }
     }
-    if (verificar.join('') === senha.join('')) {
-        console.log('SIM');
-    } else {
-        console.log('NAO');
-    }
+    console.log('SIM');
 }
 
 processData('cubos\nucuggbyos');
